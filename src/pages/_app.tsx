24e881eae3ef4555a4acb42e5f9e184ef1808298
Lucy Awrey <35198881+lucyawrey@users.noreply.github.com>
2019-12-01 16:15:41 -0500

    Added tslint support

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,21 +1,21 @@
-import React from 'react';
-import App from 'next/app';
-import Head from 'next/head';
-import { ThemeProvider } from '@material-ui/core/styles';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import theme from '../theme';
-import HeaderBar from '../components/HeaderBar';
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { ThemeProvider } from "@material-ui/core/styles";
+import App from "next/app";
+import Head from "next/head";
+import React from "react";
+import HeaderBar from "../components/HeaderBar";
+import theme from "../theme";
 
 export default class MyApp extends App {
-  componentDidMount() {
+  public componentDidMount() {
     // Remove the server-side injected CSS.
-    const jssStyles = document.querySelector('#jss-server-side');
+    const jssStyles = document.querySelector("#jss-server-side");
     if (jssStyles) {
       jssStyles.parentElement!.removeChild(jssStyles);
     }
   }
 
-  render() {
+  public render() {
     const { Component, pageProps } = this.props;
 
     return (
