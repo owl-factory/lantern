ecda807239dafc7efc8b3792795d7222d574ed75
Laura Wenning <laura.r.wenning@gmail.com>
2019-11-30 22:38:36 -0500

    Adds mongodb. Adds playground page for Laura's dev

---
+++ src/pages/laura-playground.tsx
@@ -0,0 +1,33 @@
+import { Typography } from "@material-ui/core"
+import mongodb from "mongodb"
+import React from "react"
+import Page from "../components/Page"
+
+/**
+ * A playground for Laura's development so we don't bump into each other <3
+ */
+function LauraPlayground() {
+
+  function init(): void {
+    
+  }
+
+  function getAll() {
+    mongodb.
+  }
+
+  return (
+    <Page>
+      <Typography variant="h3" paragraph>
+        Laura's Playground!
+      </Typography>
+      <Typography paragraph>
+        Here's where Laura is going to be doing her testing. &lt;3
+      </Typography>
+
+
+    </Page>
+  )
+}
+
+export default LauraPlayground;
