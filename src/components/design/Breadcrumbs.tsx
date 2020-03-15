2bea3f069732415db7582a28c6ec3e47018a601d
Laura Wenning <laura.r.wenning@gmail.com>
2020-03-15 15:12:18 +0000

    Adds easy-to-use breadcrumbs component

---
+++ src/components/design/Breadcrumbs.tsx
@@ -0,0 +1,58 @@
+import { Breadcrumbs as MuiBreadcrumbs, Link as MuiLink } from "@material-ui/core";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { def } from "../../helpers/common";
+
+interface IBreadcrumb {
+  skipLevels?: number; // the endpoint to start from
+  titles?: string[];
+}
+
+/**
+ * A simple to use breadcrumb component
+ * @param props see IBreadcrumb
+ */
+function Breadcrumbs(props: IBreadcrumb) {
+  const router = useRouter();
+  const skipLevels: number = def<number>(props.skipLevels, 0);
+  const titles: string[] = def<string[]>(props.titles, []);
+
+  function buildCrumbs() {
+    const crumbs = router.asPath.split("/");
+    const breadcrumbs: JSX.Element[] = [];
+    let uri = "";
+
+    for (let i: number = 0; i < crumbs.length; i++) {
+      const titleIndex: number = i - skipLevels;
+      uri += crumbs[i];
+      if (i < crumbs.length - 1) {
+        uri += "/";
+      }
+
+      if (titleIndex < 0) {
+        continue;
+      }
+
+      let title: string = crumbs[i];
+      if (titles.length >= titleIndex) {
+        title = titles[titleIndex];
+      }
+
+      breadcrumbs.push(
+        <Link href={uri} passHref>
+          <MuiLink color="inherit">{title}</MuiLink>
+        </Link>,
+      );
+    }
+
+    return breadcrumbs;
+  }
+
+  return (
+    <MuiBreadcrumbs aria-label="breadcrumb">
+      { buildCrumbs() }
+    </MuiBreadcrumbs>
+  );
+}
+
+export default Breadcrumbs;
--- src/pages/admin/game-system/index.tsx
+++ src/pages/admin/game-system/index.tsx
@@ -1,48 +1,40 @@
 import { useRouter } from "next/router";
 // import { Table, TableBody, TableCell, TableHead, TableRow } from "@material-ui/core";
+import Breadcrumbs from "../../../components/design/Breadcrumbs";
 import Table, { ILayoutItem } from "../../../components/design/Table";
 import Page from "../../../components/Page";
 import gamesystemJson from "./gamesystems.json";
-import content from "../../api/content";
 
-function GameSystemRows() {
-
-}
-
-function GameSystemTable() {
-
-  const layout: ILayoutItem[] = [
-    {
-      title: "",
-      component: (props: any) => {return <b>{props.content.id}</b>},
-    },
-    {
-      title: "System",
-      key: "system",
-    },
-    {
-      title: "Content",
-      key: "officialContentCount",
-    },
-    {
-      title: "Entities",
-      key: "officialEntityCount",
-    },
-    {
-      title: "Tools",
-      key: "Tools",
-    },
-  ];
-
-  return <Table layout={layout} json={JSON.stringify(gamesystemJson)}/>;
-}
+const layout: ILayoutItem[] = [
+  {
+    title: "",
+    component: (props: any) => <b>{props.content.id}</b>,
+  },
+  {
+    title: "System",
+    key: "system",
+  },
+  {
+    title: "Content",
+    key: "officialContentCount",
+  },
+  {
+    title: "Entities",
+    key: "officialEntityCount",
+  },
+  {
+    title: "Tools",
+    key: "Tools",
+  },
+];
 
 function GameSystem() {
 
   return (
     <Page>
       <h1>Admin Game System Index</h1>
-      <GameSystemTable/>
+      <Breadcrumbs skipLevels={1} titles={["Admin", "Game Systems"]}/>
+      <Table layout={layout} json={JSON.stringify(gamesystemJson)}/>
     </Page>
   );
 }
