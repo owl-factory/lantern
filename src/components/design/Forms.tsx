5f885c2453171e15ca52765e4383f32443bbf53b
Laura Wenning <laura.r.wenning@gmail.com>
2020-03-27 23:01:49 +0000

    Development of form tools

--- src/components/design/Forms.tsx
+++ src/components/design/Forms.tsx
@@ -1,8 +1,14 @@
 import {
   FormControl,
+  FormControlLabel,
+  FormLabel,
   Grid,
   Input as MuiInput,
   InputLabel,
+  MenuItem as MuiMenuItem,
+  Radio,
+  RadioGroup,
+  Select as MuiSelect,
 } from "@material-ui/core";
 import react, { Dispatch, SetStateAction } from "react";
 import { def } from "../../helpers/common";
@@ -11,7 +17,21 @@ interface IForm {
   children?: any; // The contents to place within the form
 }
 
-interface IInput {
+type Size = (boolean | 2 | 1 | 12 | 6 | "auto" | 3 | 4 | 5 | 7 | 8 | 9 | 10 | 11 | undefined);
+
+interface ISharedGrid {
+  xs?: Size;
+  sm?: Size;
+  md?: Size;
+  lg?: Size;
+  xl?: Size;
+}
+
+interface IGridItem extends ISharedGrid {
+  children?: any;
+}
+
+interface IInput extends ISharedGrid {
   id: string; // The input 'id' content
   name?: string; // The name of the input
   label?: string; // The label to display as
@@ -21,10 +41,31 @@ interface IInput {
   onChange?: (event: object) => void; // The event that occurs on change
   placeholder?: string; // A placeholder value for when the input is empty
   required?: boolean; // If this is required or not
-  type?: string; // The 
+  type?: string; // The ???
   value?: string; // The value of the input
   ariaDescribedBy?: string; // Readability setting
 }
+
+interface ISelect extends ISharedGrid {
+  id: string; // The input 'id' content
+  name?: string; // The name of the input
+  label?: string; // The label to display
+  disabled?: boolean; // If this is disabled or not
+  required?: boolean; // If this is required or not
+
+  defaultValue?: string; // The default value to use, if blank
+  includeEmpty?: boolean; // Include an empty selection
+  onChange?: (event: object) => void; // The function to use on change
+  multiple?: boolean; // True allows for multiple items to be selected
+  value?: any; // The default value to display
+
+  data?: object[]; // An array of structs containing the label and value to use
+  children?: any; // User-defined children, if that wants to be handled externally
+  labelKey?: string; // The key to use for label inputs
+  valueKey?: string; // The key of the value
+}
+
+// TODO - classes for inputs
 
 /**
  * Renders the outside of a form
@@ -32,7 +73,7 @@ interface IInput {
  */
 export function Form(props: IForm) {
   const children: JSX.Element[] = [];
-  const [data, setData] = react.useState({name: ""});
+  const [data, setData] = react.useState({});
 
   /**
    * Gets the value for a specific input
@@ -62,8 +103,8 @@ export function Form(props: IForm) {
     setData(newData);
   }
 
-  // Clones and alters the children of this Form to 
-  react.Children.toArray(props.children).forEach((child: JSX.Element) => {
+  // Clones and alters the children of this Form
+  react.Children.toArray(props.children).forEach((child: any) => {
     const newChildName = def<string>(child.props.name, child.props.id);
 
     const newChild = react.cloneElement(child, {
@@ -74,7 +115,7 @@ export function Form(props: IForm) {
 
     // Ensure this field exists in the data before render
     if (getValue(newChildName) === undefined) {
-      setFormData(newChildName, "");
+      setFormData(newChildName, def<string>(child.props.defaultValue, ""));
     }
 
     children.push(newChild);
@@ -86,6 +127,24 @@ export function Form(props: IForm) {
         {children}
       </Grid>
     </form>
+  );
+}
+
+/**
+ * Creates a standard wrapper for Grid Items
+ * @param props see IGridItem
+ */
+function GridItem(props: IGridItem) {
+  const xs = def<Size>(props.xs, 12);
+  const sm = def<Size>(props.sm, 6);
+  const md = def<Size>(props.md, sm);
+  const lg = def<Size>(props.lg, md);
+  const xl = def<Size>(props.xl, lg);
+
+  return (
+    <Grid item xl={xl} lg={lg} md={md} sm={sm} xs={xs}>
+      {props.children}
+    </Grid>
   );
 }
 
@@ -119,5 +178,103 @@ export function Input(props: IInput) {
         />
       </FormControl>
     </Grid>
+  );
+}
+
+/**
+ * Renders a select element and its options
+ * @param props see ISelect
+ */
+export function Select(props: ISelect) {
+  const name = def<string>(props.name, props.id);
+  const label = def<string>(props.label, name);
+  const disabled = def<boolean>(props.disabled, false);
+  const required = def<boolean>(props.required, false);
+
+  const data = def<object[]>(props.data, []);
+  const labelKey = def<string>(props.labelKey, "label");
+  const valueKey = def<string>(props.valueKey, "value");
+
+  let emptyElement: JSX.Element | undefined;
+  if (props.includeEmpty !== false) {
+    emptyElement = <MuiMenuItem value=""/>;
+  }
+
+  let children: any = [];
+  if (props.children === undefined) {
+    data.forEach((item: any) => {
+      children.push(<MuiMenuItem value={item[valueKey]}>{item[labelKey]}</MuiMenuItem>);
+    });
+  } else {
+    children = props.children;
+  }
+
+  return (
+    <GridItem xs={props.xs} sm={props.sm} md={props.md} lg={props.lg} xl={props.xl}>
+      <FormControl>
+        <InputLabel html-for={props.id}>{label}</InputLabel>
+        <MuiSelect
+          id={props.id}
+          name={name}
+          disabled={disabled}
+          onChange={props.onChange}
+          required={required}
+          value={props.value}
+        >
+          {emptyElement}
+          {children}
+        </MuiSelect>
+      </FormControl>
+    </GridItem>
+  );
+}
+
+export function Checkboxes(props: any) {
+  const name = def<string>(props.name, props.id);
+  const label = def<string>(props.label, name);
+
+  const checkboxes: any = [];
+
+  return (
+    <GridItem xs={props.xs} sm={props.sm} md={props.md} lg={props.lg} xl={props.xl}>
+      <FormControl component="fieldset">
+        <FormLabel html-for={props.id}>{label}</FormLabel>
+        {checkboxes}
+      </FormControl>
+    </GridItem>
+  );
+}
+
+export function RadioButtons(props: any) {
+  const name = def<string>(props.name, props.id);
+  const label = def<string>(props.label, name);
+
+  const data = def<object[]>(props.data, []);
+  const labelKey = def<string>(props.labelKey, "label");
+  const valueKey = def<string>(props.valueKey, "value");
+
+  let children: any = [];
+  if (props.children === undefined) {
+    data.forEach((item: any) => {
+    children.push(<FormControlLabel value={item[valueKey]} label={item[labelKey]} control={<Radio/>}/>);
+    });
+  } else {
+    children = props.children;
+  }
+
+  return (
+    <GridItem xs={props.xs} sm={props.sm} md={props.md} lg={props.lg} xl={props.xl}>
+      <FormControl>
+        <FormLabel html-for={props.id}>{label}</FormLabel>
+        <RadioGroup
+          id={props.id}
+          name={name}
+          onChange={props.onChange}
+          value={props.value}
+        >
+          {children}
+        </RadioGroup>
+      </FormControl>
+    </GridItem>
   );
 }
--- src/pages/admin/game-system/new.tsx
+++ src/pages/admin/game-system/new.tsx
@@ -1,19 +1,29 @@
 import { Grid, Typography } from "@material-ui/core";
 import react from "react";
 import Breadcrumbs from "../../../components/design/Breadcrumbs";
-import { Form, Input } from "../../../components/design/Forms";
+import { Checkboxes, Form, Input, RadioButtons, Select } from "../../../components/design/Forms";
 import Page from "../../../components/Page";
 
 export function NewGameSystemForm() {
   const [form, setForm] = react.useState({
-    name: "",
+    name: "Boop",
     key: "",
   });
 
+  const selectData = [
+    {label: "Dungeons and Dragons 5th Edition", value: "dnd-5e"},
+    {label: "Pathfinder 2", value: "pathfinder2"},
+    {label: "Rainbows", value: "rainbows"},
+  ];
+
   return (
     <Form>
-      <Input id="name" label="System Name" required={true} defaultValue={form.name}/>
+      <Input id="name" label="System Name" required={true} defaultValue="Test"/>
       <Input id="key" label="System Key" required={true} defaultValue={form.key}/>
+      <Select id="selectTest" label="Select Test"  data={selectData} defaultValue="rainbows"/>
+      <Checkboxes id="checkboxTest" label="Checkbox Test"></Checkboxes>
+      <RadioButtons id="radioTest" label="Radio Test"  data={selectData} defaultValue="rainbows"/>
+
     </Form>
   );
 }
