27cddb55b96f534eb472b47a7d5bc3d7b0b6a02a
Laura Wenning <laura.r.wenning@gmail.com>
2020-06-02 22:23:12 +0000

    Adds single client source and initial props in test page. Removes mongoose and all models

--- src/pages/admin/test-pages/laura-playground.tsx
+++ src/pages/admin/test-pages/laura-playground.tsx
@@ -1,34 +1,34 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+/* eslint-disable react/prop-types */
 import { useQuery } from "@apollo/react-hooks";
 import gql from "graphql-tag";
 import React from "react"
 import Page from "../../../components/design/Page";
 import { NewGameSystemForm } from "../game-system/new";
+import { client } from "../../../helpers/graphql";
 
-const characterQuery = gql`
+
+const GET_POKEMON_INFO = gql`
 {
-  characters {
-    id,
-    name
+  pokemons(first: 150) {
+    id
+    number
+    name,
+    image,
+    evolutions {
+      id,
+      number,
+      name,
+      image
+    }
   }
 }`;
 
 /**
  * A playground for Laura's development so we don't bump into each other <3
  */
-function LauraPlayground() {
-  const { data, loading, error } = useQuery(characterQuery);
-
-  function testCharacters() {
-    if (loading) {
-      return <div>Loading...</div>;
-    }
-
-    if (error) {
-      return <div>Error...</div>;
-    }
-    return <div>{JSON.stringify(data.characters)}</div>;
-  }
-
+function LauraPlayground({pokemon}: any) {
+  
   return (
     <Page>
       <p>
@@ -38,11 +38,18 @@ function LauraPlayground() {
         Here&apos;s where Laura is going to be doing her testing. &lt;3
       </p>
       <p>
-        {testCharacters()}
+        {JSON.stringify(pokemon)}
       </p>
     </Page>
   );
 }
+
+LauraPlayground.getInitialProps = async () => {
+  const { data, loading } = await client.query({query: GET_POKEMON_INFO});
+  return { pokemon: data.pokemons }
+}
+
+// export default Page3;
 
 export function testJest(a: number, b: number) {
   return a + b;
