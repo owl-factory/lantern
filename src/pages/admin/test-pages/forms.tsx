75955fdd5af8a9309028c38fdfdf97029fff9a82
Laura Wenning <laura.r.wenning@gmail.com>
2020-06-28 01:27:08 +0000

    Adds formik and yup, combining both into a basic test form

--- src/pages/admin/test-pages/forms.tsx
+++ src/pages/admin/test-pages/forms.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, Col, Form, InputGroup, Row } from "react-bootstrap";
+// import { Button, Col, Form, InputGroup, Row } from "react-bootstrap";
 import {
   BaseCheckbox,
   BaseRadioButton,
@@ -19,175 +19,48 @@ import {
 import Page from "../../../components/design/Page";
 import { defState } from "../../../helpers/tools";
 
+import { Formik, Field, Form, ErrorMessage, } from "formik";
+import * as Yup from "yup";
+
 export function TestForm(props: any) {
-  const [data, setData] = defState(props.state, props.setState, {
-    name: {nameAgain: "Boop"},
-    key: "",
-    radioButtonTest: "Thing"
-  });
-
-  const [formState, setFormState] = defState(props.formState, props.setFormState, {});
-
-  const selectData = [
-    {label: "Dungeons and Dragons 5th Edition", value: "dnd-5e"},
-    {label: "Pathfinder 2", value: "pathfinder2"},
-    {label: "Rainbows", value: "rainbows"},
-  ];
-
-  const checkboxData = [
-    {label: "Dungeons and Dragons 5th Edition", name: "dnd-5e", defaultValue: false},
-    {label: "Pathfinder 2", name: "pathfinder2", defaultValue: true},
-    {label: "Rainbows", name: "rainbows", defaultValue: false},
-  ];
-
-  const errors = {
-    checkboxText: "Error!",
-    name: "Name error",
-    radioTest: "radioTest error",
-    selectTest: "Select errors",
-
-  };
-
   return (
-    <AutoForm data={data} setData={setData} formState={formState} setFormState={setFormState} errors={errors}>
-      <h2>Text Inputs</h2>
-      <Row>
-        <Form.Group as={Col}>
-          <Form.Label>Test</Form.Label>
-          <InputGroup>
-            <Input name="name.nameAgain" label="System Name" required={true} defaultValue="Test"/>
-          </InputGroup>
-        </Form.Group>
-        <Form.Group as={Col}>
-        <Form.Label>Second Test</Form.Label>
-          <Input name="name.nameAgain" label="System Name" required={true} defaultValue="Test"/>
-        </Form.Group>
-      </Row>
-
-      <h2>Select Inputs</h2>
-      <Row>
-        <Form.Group as={Col}>
-          <Form.Label>Select Test</Form.Label>
-          <Select name="selectTest" label="Select Test"  options={selectData} defaultValue="rainbows"/>
-        </Form.Group>
-
-        <Form.Group as={Col}>
-          <Form.Label>Select Test 2</Form.Label>
-          <Select name="selectTest" label="Select Test"  options={selectData} defaultValue="rainbows"/>
-        </Form.Group>
-      </Row>
-
-      <Row>
-        <Form.Group as={Col}>
-          <Multiselect name="selectTest2" label="Multiselect Test" options={selectData}/>
-        </Form.Group>
-
-        <Form.Group as={Col}>
-          <Multiselect name="selectTest2" label="Multiselect Test" options={selectData} defaultValue={[]}/>
-        </Form.Group>
-      </Row>
-
-      <h2>Checkboxes</h2>
-      <Row>
-        <Form.Group as={Col}>
-          <Checkbox name="checkboxText" label="Checkbox Test v1" />
-          <Checkbox name="checkboxText" label="Matching Checkbox" />
-        </Form.Group>
-
-        <Form.Group as={Col}>
-          <BaseCheckbox name="basecheckboxText">
-            <Form.Check.Label>Base Checkbox Text</Form.Check.Label>
-          </BaseCheckbox>
+    <Formik
+      initialValues={{
+        email: "",
+        firstName: "",
+        lastName: "",
+      }}
+      validationSchema={Yup.object ({
+        firstName: Yup.string()
+          .max(15, "Must be 15 characters or less")
+          .required("Required"),
+        lastName: Yup.string()
+          .max(20, "Must be 20 characters or less")
+          .required("Required"),
+        email: Yup.string()
+          .email("Invalid email address")
+          .required("Required")
+      })}
+      onSubmit={(values: any) => {
+        alert(JSON.stringify(values, null, 2));
+      }}
+    >
+      <Form>
+        <label htmlFor="firstName">First Name</label>
+        <Field name="firstName" type="text"/>
+        <ErrorMessage name="firstName"/>
         
-          <BaseCheckbox name="basecheckboxText">
-            <Form.Check.Label>Matching Checkbox</Form.Check.Label>
-          </BaseCheckbox>
-        </Form.Group>
-      </Row>
+        <label htmlFor="lastName">Last Name</label>
+        <Field name="lastName" type="text"/>
+        <ErrorMessage name="lastName"/>
 
-      <h2>Radio Buttons</h2>
-      <Row>
-        <Form.Group as={Col}>
-          <RadioButton id="1" name="radioButtonTest" label="Radio Button Test v1" value="Thing"/>
-          <RadioButton id="2" name="radioButtonTest" label="Another Radio Button" value="Thing2"/>
-        </Form.Group>
+        <label htmlFor="email">Email</label>
+        <Field name="email" type="text"/>
+        <ErrorMessage name="email"/>
 
-        <Form.Group as={Col}>
-          <BaseRadioButton id="base1" name="basecheckboxText" value="boopp">
-            <Form.Check.Label>Base Checkbox Text</Form.Check.Label>
-          </BaseRadioButton>
-        
-          <BaseRadioButton id="base2" name="basecheckboxText" value="boop2">
-            <Form.Check.Label>Matching Checkbox</Form.Check.Label>
-          </BaseRadioButton>
-        </Form.Group>
-      </Row>
+      </Form>
+    </Formik>
 
-      <h2>Switches</h2>
-      <Row>
-        <Form.Group as={Col}>
-          <Switch id="swtich1" name="switchtest" label="Radio Button Test v1"/>
-          <Switch id="swtich2" name="switchtest" label="Another Radio Button"/>
-        </Form.Group>
-
-        {/* TODO - this doesn't work */}
-        <Form.Group as={Col}>
-          <BaseSwitch name="baseswitchText">
-            <Form.Check.Label>Base Checkbox Text</Form.Check.Label>
-          </BaseSwitch>
-        
-          <BaseSwitch name="baseswitchText2">
-            <Form.Check.Label>Matching Checkbox</Form.Check.Label>
-          </BaseSwitch>
-        </Form.Group>
-      </Row>
-      
-      <h2>Dates</h2>
-      <Row>
-        <Form.Group as={Col}>
-          <Date name="dateTest" label="Date Test" />
-        </Form.Group>
-
-        <Form.Group as={Col}>
-          <Date name="dateTest" label="Date Test" />
-        </Form.Group>
-      </Row>
-
-      <h2>Date Time</h2>
-      <Row>
-        <Form.Group as={Col}>
-          <DateTime name="datetime" label="Date Time Test"/>
-        </Form.Group>
-
-        <Form.Group as={Col}>
-          <DateTime name="datetime" label="Date Time Test"/>
-        </Form.Group>
-      </Row>
-
-      <h2>Time</h2>
-      <Row>
-        <Form.Group as={Col}>
-          <Time name="time" label="Time Test"/>
-        </Form.Group>
-
-        <Form.Group as={Col}>
-          <Time name="time" label="Time Test"/>
-        </Form.Group>
-      </Row>
-
-      <h2>TextArea</h2>
-      <Row>
-        <Form.Group as={Col}>
-          <TextArea name="textarea" rows={2} label="Text Area"/>
-        </Form.Group>
-
-        <Form.Group as={Col}>
-          <TextArea name="textarea" rows={2} label="Text Area"/>
-        </Form.Group>
-      </Row>
-
-      <Button variant="primary">Submit!</Button>  
-    </AutoForm>
   );
 }
 
