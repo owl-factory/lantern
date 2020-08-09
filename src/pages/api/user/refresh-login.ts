1b32172bf3680d67d85dd46204efa8fe6b5248b9
Lucy Awrey <35198881+lucyawrey@users.noreply.github.com>
2020-08-08 21:29:04 -0400

    Added documentation to authentication code, removed console logs

--- src/pages/api/user/refresh-login.ts
+++ src/pages/api/user/refresh-login.ts
@@ -3,6 +3,12 @@ import fetch from "cross-fetch";
 import { parseCookies } from "nookies";
 import { Session } from "../../../models/user";
 
+/**
+ * Api endpoint that refreshes a session token with the realm api and
+ * updates the session cookie
+ * @param req server request object
+ * @param res server response object
+ */
 export default async function RefreshLogin(req: NextApiRequest, res: NextApiResponse) {
   const cookies = parseCookies({ req });
   const tokenRes = await fetch("https://realm.mongodb.com/api/client/v2.0/auth/session", {
--- src/utilities/auth.ts
+++ src/utilities/auth.ts
@@ -1,16 +1,27 @@
 import { parseCookies } from "nookies";
 import { Session } from "../models/user";
 
+/**
+ * Utility to call the anonymous login api endpoint
+ */
 export async function anonLogin(): Promise<Session> {
   const response = await fetch("/api/user/anonymous-login", { method: "POST" });
   return await response.json();
 }
 
+/**
+ * Utility to call the google login api endpoint
+ */
 export async function googleLogin(): Promise<Session> {
   const response = await fetch("/api/user/google-login", { method: "POST" });
   return await response.json();
 }
 
+/**
+ * Utility to call the email login api endpoint
+ * @param email email address sent to the api as part of the request body
+ * @param password email address sent to the api as part of the request body
+ */
 export async function emailLogin(email: string, password: string): Promise<Session> {
   const response = await fetch("/api/user/email-login", {
     method: "POST",
@@ -23,11 +34,18 @@ export async function emailLogin(email: string, password: string): Promise<Sessi
   return await response.json();
 }
 
+/**
+ * Utility to call the refresh login api endpoint
+ */
 export async function refreshSession(): Promise<Session> {
   const response = await fetch("/api/user/refresh-login", { method: "POST" });
   return await response.json();
 }
 
+/**
+ * Utility to return current session from a cookie or, if cookie
+ * is unavailable attempt to login anonymously and return a session
+ */
 export async function getSession(): Promise<Session> {
   let cookie = parseCookies().session;
   if (cookie) {
@@ -37,6 +55,9 @@ export async function getSession(): Promise<Session> {
   }
 }
 
+/**
+ * Utility to call the logout api endpoint
+ */
 export async function logout() {
   await fetch("/api/user/logout", { method: "POST" });
 }
