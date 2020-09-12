a20783daf11cb5ef053a1455ad81c84caf25e78a
Laura Wenning <laura.r.wenning@gmail.com>
2020-09-12 15:42:30 -0400

    Adds express server and mongoose connection for testing purposes

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,4 +1,34 @@
-import meaningOfLife, {Tester} from "@reroll/model/dist/index";
-const test = new Tester();
-test.beep();
-console.log(meaningOfLife);
+import express from "express";
+import { connect, Schema } from "mongoose";
+
+const main = async () => {
+
+  // create mongoose connection
+  const mongoose = await connect(
+    process.env.MONGO_CONNECTION_STRING!, 
+    {useNewUrlParser: true, useUnifiedTopology: true}
+  );
+  await mongoose.connection;
+
+  const Character = new Schema({
+    name: String
+  })
+  
+  const CharacterModel = mongoose.model("character", Character);
+  
+  
+  // Sets up a graphql server at /graphql
+  const app = express();
+  app.listen({ port: 3001 }, () => {
+    console.log(`🚀 Server ready and listening!`);
+  })
+
+  app.get("/", async (req, res) => {
+    const chars = await CharacterModel.find({})
+    res.send(chars);
+  })
+};
+
+main().catch((error)=>{
+  console.log(error, 'error');
+})
