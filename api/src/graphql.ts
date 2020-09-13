4ba19f74de6731db98bcdd54d29d2e81d47a5a09
Lucy Awrey <35198881+lucyawrey@users.noreply.github.com>
2020-09-13 12:25:09 -0400

    Added netlify dev server

--- api/src/graphql.ts
+++ api/src/graphql.ts
@@ -10,7 +10,7 @@ const typeDefs = gql`
 // Provide resolver functions for your schema fields
 const resolvers = {
   Query: {
-    hello: () => "Hello world!",
+    hello: () => "Hello world! Laura is cute!",
   },
 };
 
