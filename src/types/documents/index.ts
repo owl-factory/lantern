07d5f4c1b69c5de28c8f102c4f613b5b7d6ab940
Laura Wenning <laura.r.wenning@gmail.com>
2021-02-20 16:12:52 +0000

    Cleaning up old and unused models and documents and pages.

--- src/types/documents/index.ts
+++ src/types/documents/index.ts
@@ -1,60 +1,38 @@
 
-export * from "./Campaign";
-export * from "./CommonContentType";
-export * from "./CommonEntityType";
 export * from "./Content";
 export * from "./ContentType";
-export * from "./CoreDocument";
 export * from "./Entity";
-export * from "./EntityLayout";
-export * from "./EntityType";
-export * from "./Ruleset";
-export * from "./Module";
+export * from "./CoreDocument";
 export * from "./Organization";
-export * from "./User";
+export * from "./Ruleset";
 
-import { Campaign, CampaignModel } from "./Campaign";
-import { CommonContentType, CommonContentTypeModel } from "./CommonContentType";
-import { CommonEntityType, CommonEntityTypeModel } from "./CommonEntityType";
 import { Content, ContentModel } from "./Content";
 import { ContentType, ContentTypeModel } from "./ContentType";
-import { CoreDocument } from "./CoreDocument";
 import { Entity, EntityModel } from "./Entity";
-import { EntityLayout, EntityLayoutModel } from "./EntityLayout";
-import { EntityType, EntityTypeModel } from "./EntityType";
+import { CoreDocument } from "./CoreDocument";
+import { Organization, OrganizationModel } from "./Organization";
 import { Ruleset, RulesetModel } from "./Ruleset";
-import { Module, ModuleModel } from "./Module";
 
 /**
  * A generic document type for any situation where we might be getting document data but
  * we're unsure of the source
  */
 export type GenericDocumentType = (
-  Campaign |
-  CommonContentType |
-  CommonEntityType |
   Content |
   ContentType |
-  CoreDocument |
   Entity |
-  EntityLayout |
-  EntityType |
-  Ruleset |
-  Module
+  CoreDocument |
+  Organization |
+  Ruleset
 );
 
 /**
  * A generic type for hitting any of the Typegoose models
  */
 export type GenericModelType = (
-  typeof CampaignModel |
-  typeof CommonContentTypeModel |
-  typeof CommonEntityTypeModel |
   typeof ContentModel |
   typeof ContentTypeModel |
   typeof EntityModel |
-  typeof EntityLayoutModel |
-  typeof EntityTypeModel |
-  typeof RulesetModel |
-  typeof ModuleModel
+  typeof OrganizationModel |
+  typeof RulesetModel
 );
