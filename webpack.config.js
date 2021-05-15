8a46a3019df5bb0094c89bf05381b2dea3978105
Lucy Awrey <35198881+lucyawrey@users.noreply.github.com>
2021-05-15 16:39:42 -0400

    Added webpack config

---
+++ webpack.config.js
@@ -0,0 +1,8 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  // [..]
+  externals: {
+    bufferutil: 'commonjs bufferutil',
+    'utf-8-validate': 'commonjs utf-8-validate',
+  },
+};
