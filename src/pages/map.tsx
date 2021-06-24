9237e24453755908a38686ec33ec7900d616a2ac
Laura Wenning <laura.r.wenning@gmail.com>
2021-06-24 19:26:13 -0400

    Initial pixijs stuff

---
+++ src/pages/map.tsx
@@ -0,0 +1,11 @@
+import { Page } from "components/design";
+import { MapBoard } from "components/reroll/maps/map";
+import React from "react";
+
+export default function MapPage() {
+  return (
+    <Page>
+      <MapBoard/>
+    </Page>
+  )
+}
