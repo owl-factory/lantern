688a8477b33669dc1dc5fb334a100e8159868cae
Lucy Awrey <35198881+lucyawrey@users.noreply.github.com>
2021-10-05 18:29:00 -0400

    Redirect test

--- next.config.js
+++ next.config.js
@@ -3,25 +3,5 @@ module.exports = {
   webpack: (config, options) => {
     config.externals.push("bufferutil", "utf-8-validate");
     return config;
-  },
-  async redirects() {
-    return [
-      {
-        source: "/dashboard",
-        destination: "/",
-        permanent: true,
-      },
-    ]
-  },
-  async rewrites() {
-    return {
-      beforeFiles: [
-        {
-          source: "/",
-          destination: "/dashboard",
-          has: [{ type: "cookie", key: "session" }],
-        },
-      ]
-    }
   }
 };
