f59f2b9623f4e02cee18d71de2229318d40846e8
Laura Wenning <laura.r.wenning@gmail.com>
2022-01-17 17:52:47 +0000

    Cleans up and fixes build issues for tests to merge in

--- src/@owl-factory/utilities/arrays.ts
+++ src/@owl-factory/utilities/arrays.ts
@@ -23,7 +23,7 @@ export function arrayToList(arr?: string[]): string {
  * @param target The object key (optionally deep nested) to scan through for unique values
  * @returns An unsorted array of unique values
  */
-export function getUniques(data: unknown[], target?: string): (number | string)[] {
+export function getUniques(data: unknown[] | undefined, target?: string): string[] {
   // Base cases that would be hit by both functions
   if (data === undefined || typeof data !== "object") { return []; }
   if (Array.isArray(data) && data.length === 0) { return []; }
@@ -39,15 +39,15 @@ export function getUniques(data: unknown[], target?: string): (number | string)[
  * Fetches the unique values that are nested within objects in an array
  */
 export function $getUniquesFromObjects(data: unknown[], target: string) {
-  const uniques: (string | number)[] = [];
+  const uniques: string[] = [];
   const uniqueEncounters: Record<string | number, boolean> = {};
 
   data.forEach((item: unknown) => {
     if (typeof item !== "object" || item === null) { return; }
     const value = read(item as Record<string, unknown>, target);
-    if (typeof value === "object" || value as (number | string) in uniqueEncounters) { return; }
-    uniqueEncounters[value as (number | string)] = true;
-    uniques.push(value as (number | string));
+    if (typeof value === "object" || value as string in uniqueEncounters) { return; }
+    uniqueEncounters[value as string] = true;
+    uniques.push(value as string);
   });
 
   return uniques;
@@ -56,11 +56,11 @@ export function $getUniquesFromObjects(data: unknown[], target: string) {
 /**
  * Fetches the unique values in an array of simple values
  */
-export function $getUniquesFromSimpleTypes(data: (string | number)[]): (number | string)[] {
-  const uniques: (string | number)[] = [];
+export function $getUniquesFromSimpleTypes(data: string []): string[] {
+  const uniques: string[] = [];
   const uniqueEncounters: Record<string | number, boolean> = {};
 
-  data.forEach((item: (string | number)) => {
+  data.forEach((item: string) => {
     if (typeof item === "object") { return; }
     if (item in uniqueEncounters) { return; }
     uniqueEncounters[item] = true;
--- src/@owl-factory/utilities/tests/arrays.spec.ts
+++ src/@owl-factory/utilities/tests/arrays.spec.ts
@@ -18,21 +18,21 @@ test("getUniques with empty array", () => {
 });
 
 test("Gets the unique elements of an array of simple values", () => {
-  const messyArray = [1, 2, 3, 2, 3, 3];
+  const messyArray = ["1", "2", "3", "2", "3", "3"];
   const uniqueArray = $getUniquesFromSimpleTypes(messyArray);
   const uniqueArrayBase = getUniques(messyArray);
-  expect(uniqueArray).toStrictEqual([1, 2, 3]);
+  expect(uniqueArray).toStrictEqual(["1", "2", "3"]);
   expect(uniqueArray).toStrictEqual(uniqueArrayBase);
 });
 
 test("Gets the unique elements of an array of objects", () => {
   const messyArray = [
-    { key: 1 },
-    { key: 1 },
-    { key: 2 },
+    { key: "1" },
+    { key: "1" },
+    { key: "2" },
   ];
   const uniqueArray = $getUniquesFromObjects(messyArray, "key");
   const uniqueArrayBase = getUniques(messyArray, "key");
-  expect(uniqueArray).toStrictEqual([1, 2]);
+  expect(uniqueArray).toStrictEqual(["1", "2"]);
   expect(uniqueArray).toStrictEqual(uniqueArrayBase);
 });
