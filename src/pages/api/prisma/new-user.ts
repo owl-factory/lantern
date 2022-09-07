ca7ea6d43f9c7eb5a6e4633b24b675ec528633b7
Lucy Awrey <35198881+lucyawrey@users.noreply.github.com>
2022-09-06 21:37:03 -0400

    Initial prisma implementation

---
+++ src/pages/api/prisma/new-user.ts
@@ -0,0 +1,33 @@
+import "reflect-metadata";
+import { NextApiRequest } from "next";
+import { CampaignLogic } from "server/logic/CampaignLogic";
+import { PrismaClient } from "@prisma/client";
+import { HTTPHandler, createEndpoint } from "@owl-factory/https/backend";
+
+const prisma = new PrismaClient();
+
+/**
+ * Fetches the information needed for the current user's dashboard page.
+ */
+export async function newUser(this: HTTPHandler, _req: NextApiRequest) {
+  const username = "test_user1";
+  const email = "test1@testing.com";
+  const password = "test_pass";
+  const user = await prisma.user.create({
+    data: {
+      username,
+      displayName: username,
+      userSecret: {
+        create: {
+            username,
+            email,
+            passwordHash: password,
+        },
+      },
+    },
+  });
+  console.log(user);
+  this.returnSuccess({ user });
+}
+
+export default createEndpoint({GET: newUser});
