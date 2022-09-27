f4b9991295cb0add8cb229f408977fcd9b736f9a
Laura Wenning <laura.r.wenning@gmail.com>
2022-09-27 14:33:23 +0000

    Fixes minor issues from merge request. Fixes issue where micro was required

--- src/graphql/apollo-client.ts
+++ src/graphql/apollo-client.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-  uri: `http://192.168.0.195:3000/api/graphql`,
+  uri: `/api/graphql`,
   cache: new InMemoryCache(),
 });
 
