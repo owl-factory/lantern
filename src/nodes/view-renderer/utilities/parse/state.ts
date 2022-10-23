c2982fd07399406472e8b2a4d3915984f4a5ef42
Laura Wenning <laura.r.wenning@gmail.com>
2022-10-23 22:18:35 +0000

    Adds the ViewRenderer and some functionality, then placeholder functions for future development

---
+++ src/nodes/view-renderer/utilities/parse/state.ts
@@ -0,0 +1,9 @@
+/**
+ * Parses the default state for the given XML document
+ * @param xmlDOM The XML DOM document to parse
+ * @returns An object containing the keys and values of the default state
+ */
+export function parseDefaultState(xmlDOM: XMLDocument): Record<string, unknown> {
+  const state: Record<string, unknown> = {};
+  return state;
+}
---
+++ src/nodes/view-renderer/utilities/parse/tabs.ts
@@ -0,0 +1,9 @@
+/**
+ * Parses the tabs into an object usable for determining the current page and state
+ * @param xmlDOM The XML DOM document to extract the tabs from
+ * @returns An object describing the different tab groups and the different pages within
+ */
+export function parseTabs(xmlDOM: XMLDocument): Record<string, any> {
+  const tabs: Record<string, any> = {};
+  return tabs;
+}
