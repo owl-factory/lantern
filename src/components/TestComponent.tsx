ac028ccf7777633024976f97e673b259c9fa6627
Laura Wenning <laura.r.wenning@gmail.com>
2023-12-24 16:48:24 -0500

    Adds Prettier and includes it into ESLint. Adds VSCode autoformat config. Adds docs on VSCode extensions.

--- src/components/TestComponent.tsx
+++ src/components/TestComponent.tsx
@@ -1,8 +1,5 @@
 function TestComponent() {
-    return (
-        <input className="bg-green-300 m-2" />
-    );
-  }
-  
+  return <input className="bg-green-300 m-2" />;
+}
+
 export default TestComponent;
-  
