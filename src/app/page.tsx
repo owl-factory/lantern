d472a8bb7e5c43e181dec94e3f3bb816ebf9a8d8
Lucy Awrey <35198881+lucyawrey@users.noreply.github.com>
2023-12-24 14:36:27 -0500

    added tailwind @apply example class

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
 function Page() {
-  return <h1 className="text-green-400 text-9xl">Hello World!</h1>;
+  return (
+    <>
+      <h1 className="text-green-400 text-9xl">Hello World!</h1>
+      <button className="example-btn ml-2">Test Button</button> 
+    </>
+  );
 }
 
 export default Page;
