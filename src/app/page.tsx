b64bae00b3fdd7b3a7fcf45e9e15b292812f12b7
Lucy Awrey <35198881+lucyawrey@users.noreply.github.com>
2023-12-24 15:03:56 -0500

    Added test component, configured absolute imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
+import TestComp from "components/TestComponent";
+
 function Page() {
   return (
     <>
       <h1 className="text-green-400 text-9xl">Hello World!</h1>
-      <button className="example-btn ml-2">Test Button</button> 
+      <button className="example-btn ml-2">Test Button</button>
+      <br />
+      <TestComp />
     </>
   );
 }
---
+++ src/components/TestComponent.tsx
@@ -0,0 +1,8 @@
+function TestComponent() {
+    return (
+        <input className="bg-green-300 m-2" />
+    );
+  }
+  
+export default TestComponent;
+  
