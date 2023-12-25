0d73f62b1830553c0131a41bdc9e6138b85800e2
Lucy Awrey <35198881+lucyawrey@users.noreply.github.com>
2023-12-24 21:54:09 -0500

    removed next-env.d.ts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,6 @@ function RootLayout({ children }: { children: React.ReactNode }) {
     <html lang="en">
       <head>
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <h1>adjkslkdjaskljdklajskldjaklsjdklajskldjaklsjdklajsdkljalkjskldjaklsjd</h1>
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="manifest" href="/site.webmanifest" />
