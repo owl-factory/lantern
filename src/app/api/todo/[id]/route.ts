7d0a43501b0d4c25d8c51a717e04c8e7d7783ffb
Lucy Awrey <35198881+lucyawrey@users.noreply.github.com>
2024-01-02 20:14:22 -0500

    added auth to todo functions

--- src/app/api/todo/[id]/route.ts
+++ src/app/api/todo/[id]/route.ts
@@ -1,4 +1,6 @@
+import { AUTH_COOKIE_NAME, auth } from "lib/authentication";
 import { database } from "lib/database";
+import { NextRequest } from "next/server";
 import type { Todo, TodoUpdate } from "types/database";
 
 interface TodoGetOptions {
@@ -9,35 +11,53 @@ interface TodoGetOptions {
 }
 
 /**
+ * /api/todo/[id]:
  * Gets a single todo list item by id.
- * @param _request - Ignored web standard request object.
+ * @param request - NextJs request object that contains the POST body and auth cookies.
  * @param options - Options object that contains needed url parameters.
  * @returns single todo list item.
  */
-export async function GET(_request: Request, options: TodoGetOptions) {
-  const todo: Todo = await database
-    .selectFrom("todo")
-    .where("id", "=", options.params.id)
-    .selectAll()
-    .executeTakeFirst();
-  return Response.json(todo);
+export async function GET(request: NextRequest, options: TodoGetOptions) {
+  const authCookie = request.cookies.get(AUTH_COOKIE_NAME);
+  if (authCookie) {
+    // Session returns null on authentication failure
+    const session = await auth.validateSession(authCookie.value);
+    if (session?.sessionId) {
+      const todo: Todo = await database
+        .selectFrom("todo")
+        .where("id", "=", options.params.id)
+        .selectAll()
+        .executeTakeFirst();
+      return Response.json(todo);
+    }
+  }
+  return Response.json("User authentication failed.", { status: 401 });
 }
 
 /**
- * Creates updates a todo list item.
- * @param request - Web standard request object that contains the POST body and auth cookies.
+ * /api/todo/[id]:
+ * Updates a todo list item.
+ * @param request - NextJs request object that contains the POST body and auth cookies.
  * @returns newly created todo item.
  */
-export async function PATCH(request: Request, options: TodoGetOptions) {
-  const todoUpdate: TodoUpdate = await request.json();
-  if (todoUpdate.id && todoUpdate.id != options.params.id) {
-    return Response.json("ID in request body does not match ID in URL parameters.", { status: 422 });
+export async function PATCH(request: NextRequest, options: TodoGetOptions) {
+  const authCookie = request.cookies.get(AUTH_COOKIE_NAME);
+  if (authCookie) {
+    // Session returns null on authentication failure
+    const session = await auth.validateSession(authCookie.value);
+    if (session?.sessionId) {
+      const todoUpdate: TodoUpdate = await request.json();
+      if (todoUpdate.id && todoUpdate.id != options.params.id) {
+        return Response.json("ID in request body does not match ID in URL parameters.", { status: 422 });
+      }
+      const result = await database
+        .updateTable("todo")
+        .set(todoUpdate)
+        .where("id", "=", options.params.id)
+        .returningAll()
+        .executeTakeFirst();
+      return Response.json(result);
+    }
   }
-  const result = await database
-    .updateTable("todo")
-    .set(todoUpdate)
-    .where("id", "=", options.params.id)
-    .returningAll()
-    .executeTakeFirst();
-  return Response.json(result);
+  return Response.json("User authentication failed.", { status: 401 });
 }
--- src/app/api/todo/route.ts
+++ src/app/api/todo/route.ts
@@ -1,22 +1,43 @@
+import { AUTH_COOKIE_NAME, auth } from "lib/authentication";
 import { database } from "lib/database";
+import { type NextRequest } from "next/server";
 import type { NewTodo, Todo } from "types/database";
 
 /**
+ * /api/todo:
  * Gets all todo list items from the database.
+ * @param request - NextJs request object that contains the POST body and auth cookies.
  * @returns array of todo list items.
  */
-export async function GET() {
-  const todo: Todo[] = await database.selectFrom("todo").selectAll().execute();
-  return Response.json(todo);
+export async function GET(request: NextRequest) {
+  const authCookie = request.cookies.get(AUTH_COOKIE_NAME);
+  if (authCookie) {
+    // Session returns null on authentication failure
+    const session = await auth.validateSession(authCookie.value);
+    if (session?.sessionId) {
+      const todos: Todo[] = await database.selectFrom("todo").selectAll().execute();
+      return Response.json(todos);
+    }
+  }
+  return Response.json("User authentication failed.", { status: 401 });
 }
 
 /**
+ * /api/todo:
  * Creates a new todo list item in the database.
- * @param request - Web standard request object that contains the POST body and auth cookies.
+ * @param request - NextJs request object that contains the POST body and auth cookies.
  * @returns newly created todo item.
  */
-export async function POST(request: Request) {
-  const newTodo: NewTodo = await request.json();
-  const result = await database.insertInto("todo").values(newTodo).returningAll().executeTakeFirst();
-  return Response.json(result);
+export async function POST(request: NextRequest) {
+  const authCookie = request.cookies.get(AUTH_COOKIE_NAME);
+  if (authCookie) {
+    // Session returns null on authentication failure
+    const session = await auth.validateSession(authCookie.value);
+    if (session?.sessionId) {
+      const newTodo: NewTodo = await request.json();
+      const todo = await database.insertInto("todo").values(newTodo).returningAll().executeTakeFirst();
+      return Response.json(todo);
+    }
+  }
+  return Response.json("User authentication failed.", { status: 401 });
 }
