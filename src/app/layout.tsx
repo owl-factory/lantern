dc5cb0d013d0a64394c4ad892be6cff7d6ca58f5
Lucy Awrey <35198881+lucyawrey@users.noreply.github.com>
2024-01-10 13:51:38 -0500

    layout fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,8 +39,8 @@ function RootLayout({ children }: { children: React.ReactNode }) {
       <head>
         <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
         <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png" />
-        <link rel="icon" type="image/icons/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/icons/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
         <link rel="mask-icon" href="/icons/safari-pinned-tab.svg" color="#5bbad5" />
       </head>
       <body className="h-full">{children}</body>
