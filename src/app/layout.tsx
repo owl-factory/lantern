9bc308f87c17b3453110244e931b9246d5a6f419
Lucy Awrey <35198881+lucyawrey@users.noreply.github.com>
2024-01-10 12:37:11 -0500

    removed extra head tags already added by next

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,16 +37,11 @@ function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className="h-full">
       <head>
-        <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
-
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="manifest" href="/manifest.webmanifest" />
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
-        <meta name="msapplication-TileColor" content="#da532c" />
       </head>
       <body className="h-full">{children}</body>
     </html>
