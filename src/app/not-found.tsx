cb14a5ac607e2cd4d2f212ce8856ed1954451fcb
Lucy Awrey <35198881+lucyawrey@users.noreply.github.com>
2024-01-10 12:50:30 -0500

    Added not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,13 +5,12 @@ import { Metadata } from "next";
  * Page metadata object, NextJs will append these values as meta tags to the <head>.
  */
 export const metadata: Metadata = {
-  title: "Offline Mode",
+  title: "Welcome",
   description: "Welcome to Lantern Tabletop",
 };
 
 /**
- * "/~offline"
- * Page component used for a fallback for any route when offline.
+ * Site 404 not found page component.
  */
 function Page() {
   return (
@@ -66,7 +65,7 @@ function Page() {
                 <a className="font-medium text-white" href="#" aria-current="page">
                   Dashboard
                 </a>
-                <a className="font-medium text-gray-400" href="#">
+                <a className="font-medium text-gray-400 hover:text-gray-500" href="/api/ping">
                   API
                 </a>
                 <a
@@ -83,30 +82,7 @@ function Page() {
 
         <main id="content" role="main">
           <div className="text-center py-10 px-4 sm:px-6 lg:px-8">
-            <h1 className="block text-2xl font-bold text-white sm:text-4xl">Lantern Tabletop is Offline</h1>
-            <p className="mt-5 mb-2 text-lg text-gray-300">
-              We do not currently support offline mode, so you will need to reconnect to the internet in order to use
-              Lantern.
-            </p>
-            <div className="mt-5 flex flex-col justify-center items-center gap-2 sm:flex-row sm:gap-3">
-              <a
-                className="w-full sm:w-auto inline-flex justify-center items-center gap-x-3.5 text-center bg-amber-400 shadow-sm text-sm font-medium rounded-md hover:text-blue-600 focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-offset-2 focus:ring-offset-slate-900 transition py-3 px-4"
-                href="https://github.com/owl-factory/lantern"
-                target="_blank"
-              >
-                <svg
-                  className="w-4 h-4"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="16"
-                  height="16"
-                  fill="currentColor"
-                  viewBox="0 0 16 16"
-                >
-                  <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
-                </svg>
-                View code on GitHub.
-              </a>
-            </div>
+            <h1 className="block text-4xl font-bold text-white sm:text-4xl">404 - Not Found</h1>
           </div>
         </main>
 
