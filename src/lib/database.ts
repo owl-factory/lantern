aa0c67fb2a81e1415b1156cb6856137376366764
Lucy Awrey <35198881+lucyawrey@users.noreply.github.com>
2024-01-15 15:16:55 -0500

    increase timeout limit for db

--- src/lib/database.ts
+++ src/lib/database.ts
@@ -9,11 +9,10 @@ import type { Database } from "types/database";
 export const pool = new Pool({
   connectionString: process.env.POSTGRES_URL,
   ssl: process.env.POSTGRES_SSL !== "false",
-  /* Default pg settings */
   port: 5432,
   max: 20, // set pool max size to 20
-  idleTimeoutMillis: 1000, // close idle clients after 1 second
-  connectionTimeoutMillis: 1000, // return an error after 1 second if connection could not be established
+  idleTimeoutMillis: 5000, // close idle clients after 5 second
+  connectionTimeoutMillis: 5000, // return an error after 5 second if connection could not be established
   maxUses: 7500, // close (and replace) a connection after it has been used 7500 times (see below for discussion)
 });
 
