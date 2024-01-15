dd032ea77d84ae91853dfe23219c8e62b19a5d9d
Lucy Awrey <35198881+lucyawrey@users.noreply.github.com>
2024-01-15 15:32:30 -0500

    updated postgres settings to work better for serverless deployments

--- src/lib/database.ts
+++ src/lib/database.ts
@@ -10,10 +10,11 @@ export const pool = new Pool({
   connectionString: process.env.POSTGRES_URL,
   ssl: process.env.POSTGRES_SSL !== "false",
   port: 5432,
+  /** This setup is for serverless functions calling a postgres pool. */
   max: 20, // set pool max size to 20
-  idleTimeoutMillis: 5000, // close idle clients after 5 second
-  connectionTimeoutMillis: 5000, // return an error after 5 second if connection could not be established
-  maxUses: 7500, // close (and replace) a connection after it has been used 7500 times (see below for discussion)
+  idleTimeoutMillis: 0, // close idle clients after 5 second
+  connectionTimeoutMillis: 0, // return an error after 5 second if connection could not be established
+  allowExitOnIdle: true, // closes idle connections
 });
 
 const dialect = new PostgresDialect({
