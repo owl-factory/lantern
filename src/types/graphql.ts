28b36effc7c96fff7b5ccff1a3b08d7efbfa7a7c
Lucy Awrey <35198881+lucyawrey@users.noreply.github.com>
2024-01-17 12:05:38 -0500

    added better typing to graphql resolvers

---
+++ src/types/graphql.ts
@@ -0,0 +1,1 @@
+export type SelectFields<T> = (keyof T)[];
--- src/utils/graphql.ts
+++ src/utils/graphql.ts
@@ -1,3 +1,6 @@
+import { type DocumentNode } from "@apollo/client";
+import { type SelectFields } from "types/graphql";
+
 /**
  * Utility function that obtains a list of field names requested in a Graphql query.
  * Only works for top level values, but that should not be a problem as we only care about
@@ -6,10 +9,7 @@
  * I could not locate type information about it so I had to set it to `unknown`.
  * @returns an array of field names requested in the GraphQL query.
  */
-export function getQueryFields(info: unknown): string[] {
-  if (!info["fieldNodes"]) {
-    return [""];
-  }
+export function getQueryFields<T>(info: DocumentNode): SelectFields<T> {
   return info["fieldNodes"].reduce((all, currentNode) => {
     all.push(
       ...currentNode.selectionSet.selections.map((selection) => {
