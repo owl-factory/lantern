9cca82d4d48ca3f727933b3b94d40f3d3014af24
Lucy Awrey <35198881+lucyawrey@users.noreply.github.com>
2024-01-17 15:05:31 -0500

    add NEXT_PUBLIC_GRAPHQL_REMOTE_URL environment variable

--- src/lib/graphql/client.ts
+++ src/lib/graphql/client.ts
@@ -2,19 +2,18 @@ import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
 import { registerApolloClient } from "@apollo/experimental-nextjs-app-support/rsc";
 import { baseUrl } from "utils/environment";
 
+const remoteUrl = process.env.NEXT_PUBLIC_GRAPHQL_REMOTE_URL;
+const isRemote = remoteUrl !== null && remoteUrl !== "";
+
+const link = isRemote ? new HttpLink({ uri: remoteUrl }) : new HttpLink({ uri: baseUrl + "/api/graphql" });
+
 /**
  * Apollo client for use in nextjs react server components.
  * We may switch to urql. https://formidable.com/open-source/urql/docs
  */
 export const getRscGqlClient = registerApolloClient(() => {
   return new ApolloClient({
+    link,
     cache: new InMemoryCache(),
-    link: new HttpLink({
-      // this needs to be an absolute url, as relative urls cannot be used in SSR
-      uri: baseUrl + "/api/graphql",
-      // you can disable result caching here if you want to
-      // (this does not work if you are rendering your page with `export const dynamic = "force-static"`)
-      // fetchOptions: { cache: "no-store" },
-    }),
   });
 }).getClient;
