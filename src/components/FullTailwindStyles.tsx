d4707f16adc03449e4271c68010261b630c3b28d
Lucy Awrey <35198881+lucyawrey@users.noreply.github.com>
2024-01-17 20:39:40 -0500

    Updated tsdoc for tailwind cdn component, made it use next/script

--- src/components/FullTailwindStyles.tsx
+++ src/components/FullTailwindStyles.tsx
@@ -1,7 +1,10 @@
+import Script from "next/script";
+
 /**
- * Pulls in all tailwind styles for testing
- * TODO - remove this in prod
+ * Pulls in the TailwindCSS CDN script for testing. This script generates required Tailwind styles dynamically
+ * as needed (perfect for things like our character sheets) but is not recommended for production.
+ * TODO - Find a better solution for supporting all Tailwind classes dynamically before we go to production.
  */
 export function FullTailwindStyles() {
-  return <script async src="https://cdn.tailwindcss.com" />;
+  return <Script async={true} src="https://cdn.tailwindcss.com" />;
 }
