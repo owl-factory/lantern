af7bdcf806d9f80d2c2629c3de1f723b22bbf07f
Laura Wenning <38322676+laura-wenning@users.noreply.github.com>
2024-01-18 21:59:58 -0500

    Revert "Starts adding code to break down expressions into their components for questies" (#338)
    This reverts commit 38e3c9ab1171c76da171073d73bd63eaa20fe9b9.

--- src/features/dynamicRender/components/ui/Text.tsx
+++ src/features/dynamicRender/components/ui/Text.tsx
@@ -1,24 +1,11 @@
-import { DynamicContext } from "features/dynamicRender/context/dynamicContext";
-import { Attribute } from "features/dynamicRender/types/attributes";
 import { NodeType } from "features/dynamicRender/types/node";
 import { RenderComponentDefinition, RenderComponentProps } from "features/dynamicRender/types/render";
-import { containsExpression } from "features/dynamicRender/utils/expression";
-import { useContext, useState } from "react";
-
-function useAttribute(attribute: Attribute) {
-  const context = useContext(DynamicContext);
-  if (attribute.isExpression === false) return attribute.value;
-
-  return "";
-}
 
 /**
  * Renders a group of Pages that can be shown one at a time
  */
-export function Text(props: RenderComponentProps<{ text: Attribute }>) {
-  const text = useAttribute(props.attributes.text);
-
-  return <>{text}</>;
+export function Text(props: RenderComponentProps) {
+  return <>{props.node.textContent.trim()}</>;
 }
 
 export const textBundle: RenderComponentDefinition = {
@@ -26,13 +13,4 @@ export const textBundle: RenderComponentDefinition = {
   nodeType: NodeType.Text,
   attributes: [],
   allowsChildren: false,
-  customParsing,
 };
-
-function customParsing(node: Node, attributes: Record<string, Attribute>) {
-  const textContent = node.textContent;
-  if (!containsExpression(textContent)) {
-    attributes.text = { value: textContent, isExpression: false };
-    return;
-  }
-}
