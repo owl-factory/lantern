fa87ae9a1a86cfe9b661c65d66786f6c4e31e257
Laura Wenning <laura.r.wenning@gmail.com>
2024-02-03 19:45:44 -0500

    Removes changes to next.config.js, adds the disable flag from MR #340

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,14 @@
-// import withSerwistInit from "@serwist/next";
+import withSerwistInit from "@serwist/next";
 
-// const withSerwist = withSerwistInit({
-//   cacheOnFrontEndNav: true,
-//   swSrc: "src/app/service-worker.ts",
-//   swDest: "public/sw.js",
-//   injectionPoint: "self.__SW_MANIFEST",
-// });
+const withSerwist = withSerwistInit({
+  cacheOnFrontEndNav: true,
+  swSrc: "src/app/service-worker.ts",
+  swDest: "public/sw.js",
+  injectionPoint: "self.__SW_MANIFEST",
+  disable: process.env.NODE_ENV === "development",
+});
 
 /** @type {import("next").NextConfig} */
 const nextConfig = {};
 
-// export default withSerwist(nextConfig);
-export default nextConfig;
+export default withSerwist(nextConfig);
