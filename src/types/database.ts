21eb79622c296f97fad801e3c26860944a56ae49
Lucy Awrey <35198881+lucyawrey@users.noreply.github.com>
2024-02-07 22:15:10 -0500

    Added data util to flatten and expand dynamic javascript objects, along with matching tests

--- src/types/database.ts
+++ src/types/database.ts
@@ -1,6 +1,13 @@
 import type { Insertable, Selectable, Updateable } from "kysely";
 import type { Key, Session, Todo, User } from "generated/database-types";
 
+/**
+ * Dynamic data type used to represent JSON data in the database with unknown keys and values.
+ * This definition is equivalent to Record\<string, string\> but with a named key.
+ * Some JSON data in the database will have a known structure that may include one or more of these dynamic data fields.
+ */
+export type Data = { [key: string]: string };
+
 export type SelectUser = Selectable<User>;
 export type NewUser = Insertable<User>;
 export type UserUpdate = Updateable<User>;
