6dfd5a6daf8b20ec5112c9382af853f7b83649a3
Laura Wenning <laura.r.wenning@gmail.com>
2024-02-09 17:41:27 -0500

    Some expression work

--- src/features/dynamicRender/components/ui/Text.tsx
+++ src/features/dynamicRender/components/ui/Text.tsx
@@ -3,13 +3,13 @@ import {
   RenderComponentDefinition,
   RenderComponentProps,
 } from "features/dynamicRender/types/render";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 /**
  * Renders a group of Pages that can be shown one at a time
  */
-export function Text(props: RenderComponentProps) {
-  const textContent = useExpression()
+export function Text(props: RenderComponentProps<TextAttributes>) {
+  const textContent = useExpression();
 
   return <>{props.node.textContent?.trim()}</>;
 }
@@ -21,13 +21,49 @@ export const textBundle: RenderComponentDefinition = {
   allowsChildren: false,
 };
 
-function useExpression(defaultValue = "") {
+function useExpression(expression: Expression | undefined, defaultValue = "") {
   const [value, setValue] = useState(defaultValue);
+  const dependencies = getExpressionDependencies(expression);
+
+  useEffect(() => {
+    if (!expression) return;
+    if (expression.type === ExpressionType.Hardcoded) {
+      setValue(expression.value);
+      return;
+    }
+    const 
+  }, dependencies);
 
   return value;
 }
 
-type TextAttributes = {
-  text: 
+/**
+ * 
+ * @param expression - The expression
+ * @returns 
+ */
+function getExpressionDependencies(expression: Expression | undefined): string[] {
+  if (!expression || expression.type === ExpressionType.Hardcoded) return [];
+  return [];
 }
 
+type TextAttributes = {
+  text: Expression;
+};
+
+type Expression = HardcodedExpression | ComputedExpression;
+
+type HardcodedExpression = {
+  type: ExpressionType.Hardcoded;
+  value: string;
+};
+
+type ComputedExpression = {
+  type: ExpressionType.Computed;
+  value: string;
+};
+
+enum ExpressionType {
+  Hardcoded,
+  Computed,
+}
