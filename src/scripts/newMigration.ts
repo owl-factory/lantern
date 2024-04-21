8fdad914203a83e0076e38a5ae2ffd897d195bc0
Lucy Awrey <35198881+lucyawrey@users.noreply.github.com>
2024-04-21 01:16:13 -0400

    new migration template string

--- src/scripts/newMigration.ts
+++ src/scripts/newMigration.ts
@@ -7,11 +7,11 @@ import { Now } from "utils/database";
 const migrationTemplate = `import { Kysely, sql } from "kysely";
 
 export async function up(db: Kysely<any>): Promise<void> {
-  await db.schema.createTable("PLACEHOLDER").addColumn("exampleArrayColumn", sql\`text[]\`);
+  await db.schema.createTable("placeholder").addColumn("exampleColumn", "text").execute();
 }
 
 export async function down(db: Kysely<any>): Promise<void> {
-  await db.schema.dropIndex("PLACEHOLDER");
+  await db.schema.dropTable("placeholder").execute();
 }
 `;
 
