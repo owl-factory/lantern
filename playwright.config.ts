24e2e25ded24b461e37dca4e402baac05ddb5065
Laura Wenning <laura.r.wenning@gmail.com>
2024-06-02 14:26:17 -0400

    Updates the shell.nix to allow for Playwright tests. Updates the playwright config, removing projects to only run chromium by default

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig, devices } from "@playwright/test";
+import { defineConfig } from "@playwright/test";
 
 /**
  * TODO remove 'dotenv' by loading env file using native NodeJs feature
@@ -37,43 +37,7 @@ export default defineConfig({
     },
   },
 
-  /* Configure projects for major browsers */
-  projects: [
-    {
-      name: "chromium",
-      use: { ...devices["Desktop Chrome"] },
-    },
-
-    {
-      name: "firefox",
-      use: { ...devices["Desktop Firefox"] },
-    },
-
-    {
-      name: "webkit",
-      use: { ...devices["Desktop Safari"] },
-    },
-
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
-  ],
+  quiet: process.env.QUIET_PLAYWRIGHT ? true : false,
 
   /* Run your local dev server before starting the tests */
   webServer: {
