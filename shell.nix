9bbffad783f76962c49c9feaa60a983207d1f6e8
Laura Wenning <38322676+laura-wenning@users.noreply.github.com>
2024-06-06 20:03:09 -0400

    Playwright updates for Nix (#348)
    * Updates the shell.nix to allow for Playwright tests. Updates the playwright config, removing projects to only run chromium by default
    
    * Organizes the deployment compose file
    
    * updated lockfile
    
    ---------
    
    Co-authored-by: Lucy Awrey <35198881+lucyawrey@users.noreply.github.com>

--- shell.nix
+++ shell.nix
@@ -1,6 +1,5 @@
 with import <nixpkgs> {};
-
-stdenv.mkDerivation {
+  stdenv.mkDerivation {
     name = "node";
     buildInputs = [
       jq
@@ -8,8 +7,15 @@ stdenv.mkDerivation {
       nodejs_20.pkgs.pnpm
     ];
 
+    nativeBuildInputs = [
+      playwright-driver.browsers
+    ];
+
     shellHook = ''
-        export PATH="$PWD/node_modules/.bin/:$PATH"      
-        alias scripts='jq ".scripts" package.json'
+      export PATH="$PWD/node_modules/.bin/:$PATH"
+      export PLAYWRIGHT_BROWSERS_PATH=${pkgs.playwright-driver.browsers}
+      export PLAYWRIGHT_SKIP_VALIDATE_HOST_REQUIREMENTS=true
+      export QUIET_PLAYWRIGHT=true
+      alias scripts='jq ".scripts" package.json'
     '';
-}
+  }
